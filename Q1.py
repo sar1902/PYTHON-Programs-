103de8bde0ae2860c556c9c128c1d54d5ff3af65
Sarthak Barde <sarthakbarde29@gmail.com>
2026-09-24 12:56:40 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,12 @@
+# Taking inputs from the user
+name = input("Enter your name:Sarthak Barde ")
+age = input("Enter your age:18 ")
+college = input("Enter your college name:RBU ")
+branch = input("Enter your branch:ECS ")
+
+# Printing the collected information
+print("\n--- Student Details ---")
+print(f"Name:    {name}")
+print(f"Age:     {age}")
+print(f"College: {college}")
+print(f"Branch:  {branch}")
