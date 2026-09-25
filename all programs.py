2bb813b2edd8623950e90843af4290045db9f283
Sarthak Barde <sarthakbarde29@gmail.com>
2026-09-25 12:30:34 +0530

    Update all programs.py

--- all programs.py
+++ all programs.py
@@ -28,7 +28,28 @@ sum = num1 + num2
 print("Sum =", sum)  
 
 
-Q4.Perform all arithmetic operations on two numbers
+Q4.Perform all arithmetic operations on two numbers 
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+
+if num2 != 0:
+    print("Division =", num1 / num2)
+    print("Floor Division =", num1 // num2)
+    print("Modulus =", num1 % num2)
+else:
+    print("Division, Floor Division, and Modulus are not possible.")
+
+print("Exponentiation =", num1 ** num2)
+
+
+Q5. Calculate area of a circle
+
+
 
 
 
