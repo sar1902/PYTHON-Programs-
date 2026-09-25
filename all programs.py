d43cbf3c9ce74c0df2ccfd5f3484a2da54d7a003
Sarthak Barde <sarthakbarde29@gmail.com>
2026-09-25 12:22:07 +0530

    Update all programs.py

--- all programs.py
+++ all programs.py
@@ -1,12 +1,16 @@
-# Taking inputs from the user
+Q1. Print name, age, college and branch
+
 name = input("Enter your name:Sarthak Barde ")
 age = input("Enter your age:18 ")
 college = input("Enter your college name:RBU ")
 branch = input("Enter your branch:ECS ")
 
-# Printing the collected information
 print("\n--- Student Details ---")
 print(f"Name:    {name}")
 print(f"Age:     {age}")
 print(f"College: {college}")
 print(f"Branch:  {branch}")
+
+
+Q2. Take name as input and greet the user 
+
